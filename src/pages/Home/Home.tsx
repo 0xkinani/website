e84b5aa842c9798093ac567fa663203816d6dd6a
0xkinani <0xkinani@proton.me>
2025-05-06 12:53:59 +0200

    added the component for recent articles for the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import { Header } from "../../components/ui/Header/Header";
 import { FeaturedBlog } from "../../components/FeaturedBlog/FeaturedBlog";
 import { SectionTitle } from "../../components/ui/SectionTitle/SectionTitle";
+import { RecentArticles } from "../../components/RecentArticles/RecentArticles";
 export const Home = () => {
   return (
     <div className="w-full">
       <Header />
       <FeaturedBlog />
       <SectionTitle />
+      <RecentArticles />
     </div>
   );
 };

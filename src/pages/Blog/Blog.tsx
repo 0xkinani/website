3b9492d197904034bf5905a6b50f9f481f5b2cdb
0xkinani <0xkinani@proton.me>
2025-05-06 18:09:01 +0200

    chore: worked on the blogs page to display all the blogs

---
+++ src/pages/Blog/Blog.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../../components/ui/Footer/Footer";
+import { Header } from "../../components/ui/Header/Header";
+import { Blogs } from "../../components/Blog/Blogs";
+export const Blog = () => {
+  return (
+    <div className="w-full">
+      <Header />
+      <Blogs />
+
+      <Footer />
+    </div>
+  );
+};
--- src/route/AllRoutes.tsx
+++ src/route/AllRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { About } from "../pages/About/About";
+import { Blog } from "../pages/Blog/Blog";
 
 const AllRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AllRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </Router>
   );

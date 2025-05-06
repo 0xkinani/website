c472df53e32d765ea5f554106c6e636e1739103d
0xkinani <0xkinani@proton.me>
2025-05-06 12:56:19 +0200

    fix: fixed rounded corners for the recent articles card

--- src/components/RecentArticles/RecentArticles.tsx
+++ src/components/RecentArticles/RecentArticles.tsx
@@ -4,11 +4,8 @@ export const RecentArticles = () => {
   return (
     <div className="w-full flex items-center justify-center py-10">
       <div className="w-[90%] flex flex-wrap justify-between gap-5">
-
-
-
         <div className="w-[48%] flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
+          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
             <img
               className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
               src={demoImagePath}
@@ -55,12 +52,8 @@ export const RecentArticles = () => {
           </div>
         </div>
 
-
-
-
-
         <div className="w-[48%] flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
+          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
             <img
               className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
               src={demoImagePath}
@@ -107,10 +100,8 @@ export const RecentArticles = () => {
           </div>
         </div>
 
-
-
         <div className="w-[48%] flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
+          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
             <img
               className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
               src={demoImagePath}
@@ -157,107 +148,8 @@ export const RecentArticles = () => {
           </div>
         </div>
 
-
         <div className="w-[48%] flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
-            <img
-              className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
-              alt=""
-            />
-
-            {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
-            </div>
-
-            {/* blog title */}
-
-            <div className="absolute bottom-0 left-0 w-full flex items-center justify-center">
-              <div className="w-[95%] left-4 bg-white border border-r-5 border-b-5 px-2   flex items-center gap-1 my-3 p-2">
-                <h3 className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                  It's Great The Government Is Tightening Gambling Rules
-                </h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full flex items-center gap-2 my-5">
-            <div>
-              <img
-                src={authorImagePath}
-                className="w-[30px] h-[30px] rounded-full shadow-2xl"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <p className="font-bold">0xkinani</p>
-
-              <p className="text-sm">November 29, 2022</p>
-            </div>
-          </div>
-
-          <div className="w-full mb-5 text-xl">
-            <p>
-              Far far away, behind the word mountains, far from the countries...
-            </p>
-          </div>
-        </div>
-
-
-        <div className="w-[48%] flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
-            <img
-              className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
-              alt=""
-            />
-
-            {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
-            </div>
-
-            {/* blog title */}
-
-            <div className="absolute bottom-0 left-0 w-full flex items-center justify-center">
-              <div className="w-[95%] left-4 bg-white border border-r-5 border-b-5 px-2   flex items-center gap-1 my-3 p-2">
-                <h3 className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                  It's Great The Government Is Tightening Gambling Rules
-                </h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full flex items-center gap-2 my-5">
-            <div>
-              <img
-                src={authorImagePath}
-                className="w-[30px] h-[30px] rounded-full shadow-2xl"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <p className="font-bold">0xkinani</p>
-
-              <p className="text-sm">November 29, 2022</p>
-            </div>
-          </div>
-
-          <div className="w-full mb-5 text-xl">
-            <p>
-              Far far away, behind the word mountains, far from the countries...
-            </p>
-          </div>
-        </div>
-
-
-        <div className="w-[48%] flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
+          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
             <img
               className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
               src={demoImagePath}

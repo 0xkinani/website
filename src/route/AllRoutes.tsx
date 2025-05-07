87f7a4edf6b2f5b3fbdaafd3aba72b50ecc82d3a
0xkinani <0xkinani@proton.me>
2025-05-07 14:40:22 +0200

    chore: prevented the markdown editor from displaying in production when users navigate to '/editor'

--- src/route/AllRoutes.tsx
+++ src/route/AllRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { About } from "../pages/About/About";
 import { Blog } from "../pages/Blog/Blog";
@@ -6,6 +6,7 @@ import { Contact } from "../pages/Contact/Contact";
 import { SingleBlog } from "../pages/Blog/SingleBlog";
 import { Editor } from "../pages/Editor/Editor";
 const AllRoutes = () => {
+  const { DEV } = import.meta.env;
   return (
     <Router>
       <Routes>
@@ -14,7 +15,8 @@ const AllRoutes = () => {
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog/:id" element={<SingleBlog />} />
-        <Route path="/editor" element={<Editor />} />
+        <Route path="/editor" element={DEV ? <Editor /> : <Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );

50bc90fa3be1c610a447a11a992e3da45cb7eeef
0xkinani <0xkinani@proton.me>
2025-05-07 13:35:11 +0200

    chore: changed all static blogs to be dynamic from the blogd.json file

--- src/components/FeaturedBlog/FeaturedBlog.tsx
+++ src/components/FeaturedBlog/FeaturedBlog.tsx
@@ -1,8 +1,16 @@
 import { FaArrowRight } from "react-icons/fa6";
+import blogs from "../../data/blogs.json";
+import { Link } from "react-router-dom";
 
 export const FeaturedBlog = () => {
-  const demoImagePath = "/images/blog/demo.jpg";
+
   const authorImagePath = "/images/blog/author.jpeg";
+  const featuredBlogId = "fee02a49-fb50-462f-84b1-3fe65e608092";
+  const featuredBlog = blogs.find((blog) => blog.id === featuredBlogId);
+
+  if (!featuredBlog) {
+    return <div>No featured blog found</div>;
+  }
 
   return (
     <div className="w-full flex items-center justify-center">
@@ -11,22 +19,22 @@ export const FeaturedBlog = () => {
           <div className="lg:w-[50%] w-full h-full border border-r-6 border-b-6 rounded-tr-xl rounded-bl-xl overflow-hidden relative">
             <img
               className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
+              src={featuredBlog.image}
               alt=""
             />
 
             {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
+            <div className={`absolute top-4 left-4 bg-${featuredBlog.category.toLowerCase()} border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1`}>
               <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
+              <h3 className="text-sm text-white">{featuredBlog.category}</h3>
             </div>
           </div>
 
           <div className="lg:w-[50%] w-full h-full flex flex-col items-center justify-center lg:px-10 px-2">
             <div className="w-full">
-              <h3 className="lg:text-5xl text-3xl font-extrabold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                No News Has Yet Reached Us About Who Will Be Attending
-              </h3>
+              <Link to={`/blog/${featuredBlog.id}`} className="lg:text-5xl text-3xl font-extrabold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
+                {featuredBlog.title}
+              </Link>
             </div>
             <div className="w-full flex items-center gap-2 my-5">
               <img
@@ -35,25 +43,28 @@ export const FeaturedBlog = () => {
                 alt=""
               />
 
-              <p className="font-bold">0xkinani</p>
+              <p className="font-bold">{featuredBlog.author}</p>
 
               <div className="w-2 h-2 bg-black rounded-full"></div>
 
-              <p>November 29, 2022</p>
+              <p>{new Date(featuredBlog.date).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}</p>
             </div>
 
             <div className="w-full mb-5 text-xl lg:block hidden">
               <p>
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live...
+                {featuredBlog.content.replace(/[^\w\s]/g, " ").slice(0, 100)}...
               </p>
             </div>
 
             <div className="w-full">
-              <button className="flex items-center gap-2 border border-r-3 border-b-3 px-10 py-2 rounded-full cursor-pointer hover:bg-black hover:text-white">
+              <Link to={`/blog/${featuredBlog.id}`} className="flex items-center gap-2 border border-r-3 border-b-3 px-10 py-2 rounded-full cursor-pointer hover:bg-black hover:text-white w-fit">
                 Read More
                 <FaArrowRight />
-              </button>
+              </Link>
             </div>
           </div>
         </div>
--- src/components/RecentArticles/RecentArticles.tsx
+++ src/components/RecentArticles/RecentArticles.tsx
@@ -1,30 +1,45 @@
+import blogs from "../../data/blogs.json";
+import { Link } from "react-router-dom";
+interface Blog {
+  id: string;
+  date: string;
+  title: string;
+  image: string;
+  content: string;
+}
+
 export const RecentArticles = () => {
   const demoImagePath = "/images/blog/demo.jpg";
   const authorImagePath = "/images/blog/author.jpeg";
+
+  const recentBlogs = blogs.slice(0, 4);
+  
   return (
     <div className="w-full flex items-center justify-center py-10">
       <div className="w-[90%] flex flex-wrap justify-between gap-5">
-        <div className="lg:w-[48%] w-full flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
+
+        {recentBlogs.length > 0 && recentBlogs.map((blog) => (
+          <div className="lg:w-[48%] w-full flex flex-col">
+            <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
             <img
               className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
+              src={blog.image}
               alt=""
             />
 
             {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
+            <div className={`absolute top-4 left-4 bg-${blog.category.toLowerCase()} border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1`}>
               <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
+              <h3 className="text-sm text-white">{blog.category}</h3>
             </div>
 
             {/* blog title */}
 
             <div className="absolute bottom-0 left-0 w-full flex items-center justify-center">
               <div className="w-[95%] left-4 bg-white border border-r-5 border-b-5 px-2   flex items-center gap-1 my-3 p-2">
-                <h3 className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                  It's Great The Government Is Tightening Gambling Rules
-                </h3>
+                <Link to={`/blog/${blog.id}`} className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
+                  {blog.title}
+                </Link>
               </div>
             </div>
           </div>
@@ -39,165 +54,26 @@ export const RecentArticles = () => {
             </div>
 
             <div>
-              <p className="font-bold">0xkinani</p>
+              <p className="font-bold">{blog.author}</p>
 
-              <p className="text-sm">November 29, 2022</p>
+              <p className="text-sm">{new Date(blog.date).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}</p>
             </div>
           </div>
 
           <div className="w-full mb-5 text-xl">
+          
             <p>
-              Far far away, behind the word mountains, far from the countries...
+              {blog.content.replace(/[^\w\s]/g, " ").slice(0, 100)}...
             </p>
           </div>
         </div>
+        ))}
 
-        <div className="lg:w-[48%] w-full flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
-            <img
-              className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
-              alt=""
-            />
-
-            {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
-            </div>
-
-            {/* blog title */}
-
-            <div className="absolute bottom-0 left-0 w-full flex items-center justify-center">
-              <div className="w-[95%] left-4 bg-white border border-r-5 border-b-5 px-2   flex items-center gap-1 my-3 p-2">
-                <h3 className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                  It's Great The Government Is Tightening Gambling Rules
-                </h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full flex items-center gap-2 my-5">
-            <div>
-              <img
-                src={authorImagePath}
-                className="w-[30px] h-[30px] rounded-full shadow-2xl"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <p className="font-bold">0xkinani</p>
-
-              <p className="text-sm">November 29, 2022</p>
-            </div>
-          </div>
-
-          <div className="w-full mb-5 text-xl">
-            <p>
-              Far far away, behind the word mountains, far from the countries...
-            </p>
-          </div>
-        </div>
-
-
-        <div className="lg:w-[48%] w-full flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
-            <img
-              className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
-              alt=""
-            />
-
-            {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
-            </div>
-
-            {/* blog title */}
-
-            <div className="absolute bottom-0 left-0 w-full flex items-center justify-center">
-              <div className="w-[95%] left-4 bg-white border border-r-5 border-b-5 px-2   flex items-center gap-1 my-3 p-2">
-                <h3 className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                  It's Great The Government Is Tightening Gambling Rules
-                </h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full flex items-center gap-2 my-5">
-            <div>
-              <img
-                src={authorImagePath}
-                className="w-[30px] h-[30px] rounded-full shadow-2xl"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <p className="font-bold">0xkinani</p>
-
-              <p className="text-sm">November 29, 2022</p>
-            </div>
-          </div>
-
-          <div className="w-full mb-5 text-xl">
-            <p>
-              Far far away, behind the word mountains, far from the countries...
-            </p>
-          </div>
-        </div>
-
-
-        <div className="lg:w-[48%] w-full flex flex-col">
-          <div className="w-full h-[400px] border border-r-6 border-b-6 rounded-tl-xl rounded-tr-xl rounded-bl-xl overflow-hidden relative">
-            <img
-              className="w-full h-full object-cover rounded-inherit hover:scale-120 transition-all duration-700 ease-in-out"
-              src={demoImagePath}
-              alt=""
-            />
-
-            {/* blog  category */}
-            <div className="absolute top-4 left-4 bg-green-400 border border-r-3 border-b-3 px-2 rounded-xl w-fit flex items-center gap-1">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <h3 className="text-sm text-white">HEALTH</h3>
-            </div>
-
-            {/* blog title */}
-
-            <div className="absolute bottom-0 left-0 w-full flex items-center justify-center">
-              <div className="w-[95%] left-4 bg-white border border-r-5 border-b-5 px-2   flex items-center gap-1 my-3 p-2">
-                <h3 className="text-4xl font-bold hover:underline hover:text-red-500 transition-all duration-700 ease-in-out cursor-pointer">
-                  It's Great The Government Is Tightening Gambling Rules
-                </h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full flex items-center gap-2 my-5">
-            <div>
-              <img
-                src={authorImagePath}
-                className="w-[30px] h-[30px] rounded-full shadow-2xl"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <p className="font-bold">0xkinani</p>
-
-              <p className="text-sm">November 29, 2022</p>
-            </div>
-          </div>
-
-          <div className="w-full mb-5 text-xl">
-            <p>
-              Far far away, behind the word mountains, far from the countries...
-            </p>
-          </div>
-        </div>
-
+        
         
       </div>
     </div>
